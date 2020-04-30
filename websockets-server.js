75d2c01322ef3f2123b73e51edcfdeddc14d14e8
mesorangerxx <lqtruongnguyen@gmail.com>
2020-04-29 17:28:27 -0700

    push

--- websockets-server.js
+++ websockets-server.js
@@ -2,19 +2,24 @@ var WebSocket = require('ws');
 var WebSocketServer = WebSocket.Server;
 var port = 3001;
 var ws = new WebSocketServer({
-  port: port,
+  port: port
 });
-
 var messages = [];
 
-console.log("websockets server started");
+console.log('websockets server started');
 
-ws.on("connection", function (socket) {
-  console.log("client connection established");
+ws.on('connection', function (socket) {
+  console.log('client connection established');
 
-  socket.on("message", function (data) {
-    console.log("message received: " + data);
+  messages.forEach(function (msg) {
+    socket.send(msg);
+  });
+
+  socket.on('message', function (data) {
+    console.log('message received: ' + data);
     messages.push(data);
-    socket.send(data);
+    ws.clients.forEach(function (clientSocket) {
+      clientSocket.send(data);
+    });
   });
 });
